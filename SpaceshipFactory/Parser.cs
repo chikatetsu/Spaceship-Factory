f7badc85f0a6415ee2f91a01eba365e9e750b4b8
Ronan Kielt <kakapo9156@gmail.com>
2024-04-15 19:25:50 +0200

    feat: implement parser and logger classes

---
+++ SpaceshipFactory/Parser.cs
@@ -0,0 +1,30 @@
+namespace SpaceshipFactory;
+
+public static class Parser
+{
+    public static void Parse(string input)
+    {
+        string[] split = input.Split(" ");
+        string command = split[0];
+        string[] args = split[1..];
+
+        switch (command)
+        {
+            case "STOCKS":
+                break;
+            case "NEEDED_STOCKS":
+                break;
+            case "INSTRUCTIONS":
+                break;
+            case "VERIFY":
+                break;
+            case "PRODUCE":
+                break;
+            case "":
+                break;
+            default:
+                Logger.PrintError($"`{command}` is not a known command");
+                break;
+        }
+    }
+}
