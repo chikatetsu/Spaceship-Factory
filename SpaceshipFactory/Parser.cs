a2c9eb3df325e8f3b04fd69c9ca9ee2ba2de59f5
Ronan Kielt <kakapo9156@gmail.com>
2024-04-28 00:21:12 +0200

    feat: verify functions for every command

--- SpaceshipFactory/Parser.cs
+++ SpaceshipFactory/Parser.cs
@@ -1,8 +1,8 @@
 namespace SpaceshipFactory;
 
-public static class Parser
+public class Parser
 {
-    public static void Parse(string input)
+    public void Parse(string input)
     {
         string[] split = input.Split(" ");
         string command = split[0];
@@ -11,26 +11,25 @@ public static class Parser
         switch (command)
         {
             case "STOCKS":
-                if (args.Length > 0)
+                if (IsStocksCommandValid(args.Length))
                 {
-                    Logger.PrintError("The command STOCKS does not take arguments");
-                    break;
+                    Logger.PrintResult(Stock.GetStocks());
                 }
-                Logger.PrintResult(Stock.GetStocks());
                 break;
             case "NEEDED_STOCKS":
+                if (IsNeededStocksCommandValid(args)) { }
                 break;
             case "INSTRUCTIONS":
+                if (IsInstructionsCommandValid(args)) { }
                 break;
             case "VERIFY":
+                if (IsVerifyCommandValid(args.Length)) { }
                 break;
             case "PRODUCE":
-                if (args.Length < 2)
+                if (IsProduceCommandValid(args))
                 {
-                    Logger.PrintError("PRODUCE command expects at least 2 arguments");
-                    break;
+                    ProductionManager.Produce(args);
                 }
-                ProductionManager.Produce(args);
                 break;
             case "":
                 break;
@@ -39,4 +38,106 @@ public static class Parser
                 break;
         }
     }
+
+
+    private bool IsStocksCommandValid(int argsLength)
+    {
+        if (argsLength != 0)
+        {
+            Logger.PrintError("STOCKS command does not expect any arguments");
+            return false;
+        }
+        return true;
+    }
+
+
+    private bool IsNeededStocksCommandValid(IReadOnlyList<string> args)
+    {
+        if (args.Count == 0)
+        {
+            Logger.PrintError("NEEDED_STOCKS command expects at least 2 argument");
+            return false;
+        }
+        if (args.Count % 2 != 0)
+        {
+            Logger.PrintError("NEEDED_STOCKS command expects an even number of arguments");
+            return false;
+        }
+        for (int i = 0; i < args.Count; i += 2)
+        {
+            if (!int.TryParse(args[i], out _))
+            {
+                Logger.PrintError($"`{args[i]}` is not a valid quantity");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private bool IsInstructionsCommandValid(IReadOnlyList<string> args)
+    {
+        if (args.Count == 0)
+        {
+            Logger.PrintError("INSTRUCTIONS command expects at least 2 argument");
+            return false;
+        }
+        if (args.Count % 2 != 0)
+        {
+            Logger.PrintError("INSTRUCTIONS command expects an even number of arguments");
+            return false;
+        }
+        for (int i = 0; i < args.Count; i += 2)
+        {
+            if (!int.TryParse(args[i], out _))
+            {
+                Logger.PrintError($"`{args[i]}` is not a valid quantity");
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    private bool IsVerifyCommandValid(int argsLength)
+    {
+        if (argsLength < 1)
+        {
+            Logger.PrintError("VERIFY command expects at least one argument");
+            return false;
+        }
+        return true;
+    }
+
+
+    private bool IsProduceCommandValid(IReadOnlyList<string> args)
+    {
+        if (args.Count == 0)
+        {
+            Logger.PrintError("PRODUCE command expects at least 2 argument");
+            return false;
+        }
+        if (args.Count % 2 != 0)
+        {
+            Logger.PrintError("PRODUCE command expects an even number of arguments");
+            return false;
+        }
+        for (int i = 0; i < args.Count; i += 2)
+        {
+            if (!int.TryParse(args[i], out _))
+            {
+                Logger.PrintError($"`{args[i]}` is not a valid quantity");
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Si la commande est incorrecte, le résultat sera affiché sous la forme :
+    // ERROR Message
+    // Où Message indique pourquoi la commande est incorrecte
+    // Si la commande est valide et que le stock est suffisant, le résultat sera :
+    // AVAILABLE
+    // Si la commande est valide mais que le stock n’est pas suffisant, le résultat sera :
+    // UNAVAILABLE
 }
