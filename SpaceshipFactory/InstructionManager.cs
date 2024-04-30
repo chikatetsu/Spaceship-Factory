3f039adfe8c508161117d6fedd4f7f1e8cca0162
Charles <charlescretois@gmail.com>
2024-04-30 16:36:18 +0200

    wip: added every ship

--- SpaceshipFactory/InstructionManager.cs
+++ SpaceshipFactory/InstructionManager.cs
@@ -21,9 +21,10 @@ namespace SpaceshipFactory
 
                 if (!int.TryParse(quantityArg, out int newQuantity) || newQuantity < 1)
                 {
-                    quantity = newQuantity;
                     continue;
                 }
+
+                quantity = int.Parse(quantityArg);
 
                 Spaceship? spaceship = ShipModels.Find(spaceship => spaceship.Name == modelArg);
 
@@ -33,19 +34,51 @@ namespace SpaceshipFactory
                     continue;
                 }
 
-                Instruction[] instructions = new Instruction[]
-                {
-                    new Instruction("GET_OUT_OF_STOCK", "1 Hull_HE1"),
-                    new Instruction("GET_OUT_OF_STOCK", "1 Engine_EE1"),
-                    new Instruction("GET_OUT_OF_STOCK", "1 Wings_WE1"),
-                    new Instruction("GET_OUT_OF_STOCK", "1 Thruster_TE1"),
-                    new Instruction("ASSEMBLE", "TMP1 Hull_HS1 Engine_HS1"),
-                    new Instruction("ASSEMBLE", "TMP1 Wings_HS1"),
-                    new Instruction("ASSEMBLE", "TMP3[TMP1, Wings_HS1] Thruster_HS1"),
-                    new Instruction("ASSEMBLE", "TMP3 Thruster_HS1")
-                };
+                Instruction[] instructions = new Instruction[0];
 
-                for(int j = 0; j <= quantity; j++)
+
+                switch (spaceship.Name)
+                {
+                    case "Explorer":
+                        instructions = new Instruction[]
+                        {
+                            new Instruction("GET_OUT_OF_STOCK", "1 Hull_HE1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Engine_EE1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Wings_WE1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Thruster_TE1"),
+                            new Instruction("ASSEMBLE", "TMP1 Hull_HE1 Engine_EE1"),
+                            new Instruction("ASSEMBLE", "TMP1 Wings_WE1"),
+                            new Instruction("ASSEMBLE", "TMP3[TMP1, Wings_WE1] Thruster_TE1"),
+                        };
+                        break;
+                    case "Speeder":
+                        instructions = new Instruction[]
+                        {
+                            new Instruction("GET_OUT_OF_STOCK", "1 Hull_HS1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Engine_ES1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Wings_WS1"),
+                            new Instruction("GET_OUT_OF_STOCK", "2 Thruster_TS1"),
+                            new Instruction("ASSEMBLE", "TMP1 Hull_HS1 Engine_ES1"),
+                            new Instruction("ASSEMBLE", "TMP1 Wings_WS1"),
+                            new Instruction("ASSEMBLE", "TMP3[TMP1, Wings_WS1] Thruster_TS1"),
+                            new Instruction("ASSEMBLE", "TMP3 Thruster_TS1")
+                        };
+                        break;
+                    case "Cargo":
+                        instructions = new Instruction[]
+                        {
+                            new Instruction("GET_OUT_OF_STOCK", "1 Hull_HC1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Engine_EC1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Wings_WC1"),
+                            new Instruction("GET_OUT_OF_STOCK", "1 Thruster_TC1"),
+                            new Instruction("ASSEMBLE", "TMP1 Hull_HC1 Engine_EC1"),
+                            new Instruction("ASSEMBLE", "TMP1 Wings_WC1"),
+                            new Instruction("ASSEMBLE", "TMP3[TMP1, Wings_WC1] Thruster_TC1"),
+                        };
+                        break;
+                }
+
+                for (int j = 0; j < quantity; j++)
                 {
                     Logger.PrintInstruction("\nPRODUCING", $"{spaceship.Name} {j+1}");
                     foreach (Instruction instruction in instructions)
